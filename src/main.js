1ba986269eb5698cbe38589278e5e617eb7e55f5
RubisRodri <rubisrodri@gmail.com>
2022-06-14 12:53:52 -0500

    cambios en test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,58 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { createCard } from './data.js';
+//import pokemon from './data/pokemon/pokemon.js';
+//import pokemon from './data/pokemon/pokemon.js';
+//import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log ("hola")
-console.log(example, data);
+//mostrando los pokemones 
+const myPokemons = data.pokemon.map((pokemon)=> createCard(pokemon));
+
+const container = document.querySelector(".container-pokemon");
+
+container.innerHTML = myPokemons.join("");
+
+//filtrando de forma ascendente. de A a la Z
+const nombreAz = data.pokemon;
+
+nombreAz.sort((a, b) =>{
+
+    if( a.name < b.name){
+        return -1;
+    }
+    if (a.name > b.name){
+        return 1;
+    }
+        return 0;
+  });
+
+  console.log(nombreAz);
+// tambien lo podemos hacer de esta forma return a - b
+//console.log(nombreAz);
+
+const nombreZa = nombreAz;
+
+ nombreZa.sort((a, b) =>{
+    if( a.name > b.name){
+        return -1;
+    }
+    if ( a.name < b.nombre){
+        return 1;
+    }
+        return 0;
+});
+
+  console.log(nombreZa);
+
+
+
+
+// ahora filtramos de forma decendente Z a la A
+
+
+
+// tambien lo podriamos hacer return b - a;
+
+//console.log(createCard);
+
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,41 @@
-import { example, anotherExample } from '../src/data.js';
+import { createCard, anotherExample } from '../src/data.js';
 
 
-describe('example', () => {
+describe('createCard', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof createCard).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns string', () => {
+    const pokemon = [{
+      "num": "001",
+      "name": "bulbasaur",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "about": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+      "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+      "size": {
+        "height": "0.71 m",
+        "weight": "6.9 kg"},
+      "type":[
+          "grass",
+          "poison"
+      ]}]
+    expect(createCard(pokemon)).toBe(
+    `<section class="info-card-pokemon"> 
+      <div class="img-pokemon">
+       <img src="https://www.serebii.net/pokemongo/pokemon/001.png">
+     </div>
+     <div class="data-pokemon">   
+       <span>"bulbasaur"</span>
+       <span>"001"</span>
+       <span>Tipo:"type":["grass","poison"]
+       </span>
+     </div>
+   </section>`
+    );
   });
 });
 
