462fb4f7f5897e9af80ceaef7f8abc089383a6f2
RubisRodri <rubisrodri@gmail.com>
2022-06-21 10:29:42 -0500

    ordenado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createCard } from './data.js';
+import { createCard, ordenarAz, ordenarZa } from './data.js';
 //import pokemon from './data/pokemon/pokemon.js';
 //import pokemon from './data/pokemon/pokemon.js';
 //import data from './data/lol/lol.js';
@@ -12,28 +12,23 @@ const container = document.querySelector(".container-pokemon");
 
 container.innerHTML = myPokemons.join("");
 
-//ordenando  de forma ascendente. de A a la Z
-const nombreAz = data.pokemon;
-document.querySelector(".btn-sort").addEventListener("click", ordenarAz);
+ //ordenando  de forma ascendente. de A a la Z
+const pokemonAsc = [...data.pokemon]
 
-nombreAz.sort((a, b)=>{
-     if(a.name < b.name){
-        return -1;
-     }
-     if (a.name > b.name){
-       return 1;
-     }
-        return 0;
-    }
-);
-
-function ordenarAz(){
-    console.log(nombreAz);
-};
-
-  // ordenamos en for decendiente de Z a la A.
+document.querySelector(".btn-sort-asc").addEventListener("click", () => {
+    container.innerHTML = ordenarAz(pokemonAsc);
+});
 
 
+//ordenando en forma  decendiente de Z a la A
+
+  const pokemonsDes = [...data.pokemon]
+  
+  document.querySelector(".btn-sort-desc").addEventListener("click", () => {
+    container.innerHTML = ordenarZa(pokemonsDes)
+  });
 
 
-
+ 
+ 
+    
