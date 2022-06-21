4b4b8601ba8bb0e2419fac2eef636283c3df2c96
karlavasquez8 <83799832+karlavasquez8@users.noreply.github.com>
2022-06-21 11:13:10 -0500

    feat:Filtrado con DOM

--- src/main.js
+++ src/main.js
@@ -1,39 +1,71 @@
-import { createCard } from './data.js';
-//import pokemon from './data/pokemon/pokemon.js';
-//import pokemon from './data/pokemon/pokemon.js';
-//import data from './data/lol/lol.js';
+import { createCard, ordenarAz,ordenarZa,filters, filtersItems, createButton} from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-//mostrando los pokemones 
-const myPokemons = data.pokemon.map((pokemon)=> createCard(pokemon));
+//Agregamos tarjetas de Pokemones
+const container = document.querySelector('.container-pokemon');
 
-const container = document.querySelector(".container-pokemon");
+const myPokemons = data.pokemon.map((pokemon) => createCard(pokemon) );
 
 container.innerHTML = myPokemons.join("");
+//se agrega el join para convertirlo  un string, porque la iteracion del map devuelve un array
 
-//ordenando  de forma ascendente. de A a la Z
-const nombreAz = data.pokemon;
-document.querySelector(".btn-sort").addEventListener("click", ordenarAz);
+ //ordenando  de forma ascendente. de A a la Z
+const pokemonAsc = [...data.pokemon]
 
-nombreAz.sort((a, b)=>{
-     if(a.name < b.name){
-        return -1;
-     }
-     if (a.name > b.name){
-       return 1;
-     }
-        return 0;
-    }
-);
+document.querySelector(".btn-sort-asc").addEventListener("click", () => {
+    container.innerHTML = ordenarAz(pokemonAsc);
+});
+//ordenando en forma  decendiente de Z a la A
 
-function ordenarAz(){
-    console.log(nombreAz);
-};
+const pokemonsDes = [...data.pokemon]
+  
+document.querySelector(".btn-sort-desc").addEventListener("click", () => {
+  container.innerHTML = ordenarZa(pokemonsDes)
+});
 
-  // ordenamos en for decendiente de Z a la A.
+// creando eventos para filtrar
+
+const filter = document.querySelector('.btn-filter');
+filter.addEventListener("click",addModalFilter)
+
+function addModalFilter(){
+    const { types,resistant:resistants, weaknesses } = filtersItems.getFiltersItems(data.pokemon);
+
+    const containerOptionType = document.querySelector(".type");
+    const myFilterTypes = types.map((type) => createButton(type) );
+    containerOptionType.innerHTML = myFilterTypes.join("");
+
+    const containerOptionResistant =document.querySelector(".resistant");
+    const myFiltersResistant = resistants.map((resistant) => createButton(resistant) );
+    containerOptionResistant.innerHTML=myFiltersResistant.join("");
+
+    const containerOptionWeaknesses =  document.querySelector(".weaknesses")
+    const myFilterWeaknesse = weaknesses.map((weaknesses)=> createButton(weaknesses));
+    containerOptionWeaknesses.innerHTML= myFilterWeaknesse.join("");
+
+    const openModal = document.querySelector(".filter-sheet");
+    openModal.classList.add("show-filter");
+}
+
+
+const btnCloseFilter = document.querySelector('.btn-close-filter');
+btnCloseFilter.addEventListener("click",closeModal)
+
+function closeModal (){
+    const btnClose = document.querySelector(".filter-sheet");
+    btnClose.classList.remove("show-filter")
+}
 
 
 
 
+//se utiliza el objeto getFiltersItems que viene desde el data exportado con el getPokemonType que 
+//es una funcion que tiene como argumento el data.pokemon ya que se esta trayendo ess datos del archivo pokemon.js
+ //console.log (filtersItems.getFiltersItems(data.pokemon)) 
 
+// const buttonFilter= document.querySelector(".btn-meniFil");
+// buttonFilter.addEventListener("click",pokemonType)
+
+// }
+
+// carType.appendChild(createCarType);
